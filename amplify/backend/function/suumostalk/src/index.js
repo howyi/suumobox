78df21823c87e80e201e249db6dbad4bfb9a71e0
howyi <howyi.lq@gmail.com>
2019-08-16 13:35:20 +0900

    コメントを追加

--- amplify/backend/function/suumostalk/src/index.js
+++ amplify/backend/function/suumostalk/src/index.js
@@ -9,10 +9,12 @@ const AWS = require('aws-sdk');
 const S3 = new AWS.S3();
 
 const { IncomingWebhook } = require('@slack/webhook');
+// Lambdaの環境変数からSlackのIncoming WebhookのURLを取得する
 const webhook = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL);
 
 const Twitter = require('twitter');
 
+// Lambdaの環境変数からTwitterの設定を取得する
 const twitterClient = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -25,6 +27,7 @@ if(process.env.ENV && process.env.ENV !== "NONE") {
     Bucket = Bucket + '-' + process.env.ENV;
 }
 
+// ① CloudWatch Eventsで定期的に実行される想定
 exports.handler = async function (event, context) { //eslint-disable-line
 
     let param = {
@@ -33,6 +36,7 @@ exports.handler = async function (event, context) { //eslint-disable-line
         include_rts: false,
     };
     try {
+        // S3に最新取得ツイートIDが入っているか確認し、入っていた場合はそれ以降のツイートしか取得しないようにする
         const object = await S3.getObject({
             Bucket,
             Key: 'since_id',
@@ -43,15 +47,15 @@ exports.handler = async function (event, context) { //eslint-disable-line
         console.log(e)
     }
 
+    // ② twitterへ取得APIを実行する
     const tweets = await twitterClient.get('statuses/user_timeline', param);
 
+    // ④ 取得したツイートをログへ投げておく
     console.log(tweets);
 
     let nextSinceId;
     for (let tweet of tweets) {
         let expanded_text = tweet.text;
-
-        console.log(tweet.extended_entities.media);
 
         for (let media of tweet.extended_entities.media) {
             expanded_text = expanded_text.replace(
@@ -60,6 +64,7 @@ exports.handler = async function (event, context) { //eslint-disable-line
             )
         }
 
+        // ④ SlackのIncoming Webhookで設定したチャンネルにツイートを投稿する
         await webhook.send({
             text:  expanded_text,
             username: tweet.user.name,
@@ -74,6 +79,7 @@ exports.handler = async function (event, context) { //eslint-disable-line
     }
 
     if (nextSinceId) {
+        // ③ 最新のツイートIDをS3へ保存する
         await S3.putObject({
             Bucket,
             Key: 'since_id',
@@ -81,6 +87,4 @@ exports.handler = async function (event, context) { //eslint-disable-line
             Body: nextSinceId
         }).promise();
     }
-
-    context.done(null, 'Hello World'); // SUCCESS with message
 };
